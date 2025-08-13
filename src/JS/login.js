0b01662fadb8944e0a3d9e6d34a263b403f123fe
Ahmed22193 <Ahmed221050193@gmail.com>
2025-08-13 23:01:52 +0300

    update login and home page

---
+++ src/JS/login.js
@@ -0,0 +1,53 @@
+document.getElementById("myForm").addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  const btnSubmit = document.getElementById("btn-sub");
+  event.preventDefault();
+  btnSubmit.innerHTML = `Loading...`;
+  btnSubmit.disabled = true;
+  const phone = document.getElementById("Phone").value.trim();
+  const password = document.getElementById("Password").value.trim();
+  const message = document.getElementById("message");
+  const formData = {
+    phone,
+    password,
+  };
+  fetch("https://tamni.vercel.app/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        btnSubmit.innerHTML = `Done`;
+        message.innerHTML = `login successful! Redirecting...`;
+        setTimeout(()=>{
+            window.location.href = `../../../index.html`;
+        },3000)
+      } else if (response.status === 401) {
+        btnSubmit.innerHTML = `submit`;
+        btnSubmit.disabled = false;
+        message.innerHTML = `Invalid phone or password. Please try again.`;
+      } else if (response.status === 404) {
+        message.innerHTML = `User not found. Please check your phone number.`;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+      const token = data.data;
+      localStorage.setItem("token", token);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  console.log("Form Data:", formData);
+}
+
+
+
+
+
+
