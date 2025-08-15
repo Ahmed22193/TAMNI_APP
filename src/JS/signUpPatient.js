c1e78bb0218d9d1d876bb6bb35f2a60bdb040955
Ahmed22193 <Ahmed221050193@gmail.com>
2025-08-15 23:32:09 +0300

    add patient signup

---
+++ src/JS/signUpPatient.js
@@ -0,0 +1,94 @@
+document.getElementById("myForm").addEventListener("submit", handleSubmit);
+
+function validateEgyptianPhone(phone) {
+  const egyptianPhoneRegex = /^(010|011|012|015)[0-9]{8}$/;
+  return egyptianPhoneRegex.test(phone);
+}
+document.getElementById("phone").addEventListener("input", function () {
+  if (!validateEgyptianPhone(this.value)) {
+    document.getElementById("error-message").innerText = "رقم الهاتف غير صحيح";
+    document.getElementById("error-message").style.opacity = 1;
+    document.getElementById("error-message").style.transform =
+      "translateX(-40px)";
+  } else {
+    document.getElementById("error-message").innerText = "";
+    document.getElementById("error-message").style.opacity = 0;
+    document.getElementById("error-message").style.transform = "translateX(0)";
+  }
+});
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const firstName = document.getElementById("firstName").value.trim();
+  const middleName = document.getElementById("middleName").value.trim();
+  const lastName = document.getElementById("lastName").value.trim();
+  const government = document.getElementById("government").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const selectedGender = document.querySelector(
+    'input[name="gender"]:checked'
+  ).value;
+  const api_message = document.getElementById("api-message");
+  if (password !== confirmPassword) {
+    document.getElementById("error-message").innerText =
+      "كلمة السر غير متطابقة";
+    document.getElementById("error-message").style.opacity = 1;
+    document.getElementById("error-message").style.transform =
+      "translateX(-40px)";
+    return;
+  }
+  document.getElementById("error-message").innerText = "";
+  document.getElementById("error-message").style.opacity = 0;
+
+  document.getElementById("submitBtn").innerText = "...جاري الارسال";
+  document.getElementById("submitBtn").disabled = true;
+
+  const formData = {
+    firstName,
+    middleName,
+    lastName,
+    government,
+    phone,
+    password,
+    userType: "PATIENT",
+    gender: selectedGender,
+  };
+
+  fetch("https://tamni.vercel.app/api/auth/signUp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(async (response) => {
+      const data = await response.json();
+
+      if (!response.ok) {
+        const errorMsg =
+          data.err || data.message || data.error || "حدث خطأ في الإرسال";
+        throw new Error(errorMsg);
+      }
+      return data;
+    })
+    .then((data) => {
+      console.log("تم التسجيل بنجاح:", data);
+      document.getElementById("submitBtn").innerText = "تم التسجيل بنجاح";
+      api_message.classList.remove("activ-error");
+      api_message.classList.add("activ-seccess");
+      api_message.innerText = "تم تسجيلك بنجاح";
+      setTimeout(() => {
+        document.getElementById("myForm").reset();
+        window.location.href = "login.html"; ///////////
+      }, 3000); // بعد 3 ثواني هيروح على صفحة تسجيل الدخول
+    })
+    .catch((error) => {
+      // هنا بتعرض رسالة الخطأ اللي جايه من الـ API، مثلاً "الرقم مسجل بالفعل"
+      document.getElementById("submitBtn").innerText = "sign Up";
+      document.getElementById("submitBtn").disabled = false;
+      api_message.innerText = error.message;
+      api_message.classList.remove("activ-seccess");
+      api_message.classList.add("activ-error");
+    });
+}
