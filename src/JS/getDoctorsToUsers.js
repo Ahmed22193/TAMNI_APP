89d952ac4d85b1ffc204052bff60d11e7ea7e513
Ahmed22193 <Ahmed221050193@gmail.com>
2025-08-22 18:18:59 +0300

    add doctor to favorites

--- src/JS/getDoctorsToUsers.js
+++ src/JS/getDoctorsToUsers.js
@@ -10,6 +10,14 @@ function activeReload() {
   document.body.appendChild(loadingOverlay);
 }
 activeReload();
+let allDoctors = [];
+
+let favorites;
+if (localStorage.getItem("favorites") !== null) {
+  favorites = JSON.parse(localStorage.getItem("favorites"));
+} else {
+  favorites = [];
+}
 
 const apiURL = "https://tamni.vercel.app/api/doctor/Doctors";
 
@@ -23,6 +31,7 @@ fetch(apiURL)
   .then((data) => {
     document.querySelector(".loading-overlay").remove();
     cards(data.data);
+    allDoctors.push(...data.data);
   })
   .catch((error) => {
     console.error("Error:", error);
@@ -35,7 +44,7 @@ function cards(data) {
     cardsContainer.innerHTML += `
       <div class="card">
           <img src="../../src/images/doctor.jpg" alt="Doctor" />
-          <div class="favorite"><button onclick="AddToFavorites(this)">♡</button></div>
+          <div class="favorite"><button onclick="AddToFavorites('${doctor._id}')">♡</button></div>
           <div class="card-content">
             <h4>Dr : ${fullName}</h4>
             <p>📍${doctor.government} : ${doctor.address}<br /><span>${doctor.specialest}</span></p>
@@ -51,4 +60,18 @@ function requestConsultation(doctorId) {
   window.location.href = `../../src/JS/createConsultation.js?doctorId=${doctorId}`;
   window.location.href = `../pages/patient/createConsultation.html?doctorId=${doctorId}`;
 }
-function AddToFavorites(button) {}
+
+function AddToFavorites(doctorId) {
+  let i = allDoctors.findIndex((e) => e._id == doctorId);
+  if (i === -1) return alert("Doctor not found");
+  let doctorData = allDoctors[i];
+
+  let findDoctorInLocalStorage = favorites.findIndex((e) => e._id == doctorId);
+
+  if (findDoctorInLocalStorage !== -1) {
+    alert("doctor is already in favorites");
+  } else {
+    favorites.push(doctorData);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }
+}
